40d36e78a82e30d19bc67b9bbdd19c6848c7e5c3
vivek <sankaliyavivek9797@gmail.com>
2025-03-28 16:51:05 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@
     app.use(cookieParser());
     app.use(express.json());
     app.use(cors({
-        origin: ['http://localhost:5173',"https://sankaliyavivek.github.io/kit/"],
+        origin: ["https://sankaliyavivek.github.io"],
         credentials: true
     }));
 
--- frontendproject/src/socket.js
+++ frontendproject/src/socket.js
@@ -7,6 +7,7 @@ const SOCKET_API = import.meta.env.VITE_SOCKET_API_URL
 const socket = io(SOCKET_API, {
   withCredentials: true,
   transports: ["websocket", "polling"],
+  secure: true,
   auth: { token },  // ✅ Use auth instead of extraHeaders
 });
 
