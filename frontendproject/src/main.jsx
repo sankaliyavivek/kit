b3712eea8881121d7fb5fbcda33bb6606f015b7e
vivek <sankaliyavivek9797@gmail.com>
2025-03-28 15:28:00 +0530

    sa

--- frontendproject/src/main.jsx
+++ frontendproject/src/main.jsx
@@ -6,7 +6,7 @@ import  { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/kit/">
       <App></App>
     </BrowserRouter>
     
